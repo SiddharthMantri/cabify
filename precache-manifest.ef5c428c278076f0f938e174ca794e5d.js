411e146073088c42a3c3e33e9fc6600db7b63a0b
SiddharthMantri <siddharthmantri1@gmail.com>
2019-11-18 10:23:04 -0500

    Updates

---
+++ precache-manifest.ef5c428c278076f0f938e174ca794e5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9762f1e206113065bc54bff1ee0d56d",
+    "url": "/cabify/index.html"
+  },
+  {
+    "revision": "8a02e91dab34106acef7",
+    "url": "/cabify/static/css/main.ba9456da.chunk.css"
+  },
+  {
+    "revision": "fc8eb912cdff0b7681c9",
+    "url": "/cabify/static/js/2.b3b215c8.chunk.js"
+  },
+  {
+    "revision": "8a02e91dab34106acef7",
+    "url": "/cabify/static/js/main.c64f8f4f.chunk.js"
+  },
+  {
+    "revision": "f6e64a3ba3f5c40371d7",
+    "url": "/cabify/static/js/runtime-main.c93b9376.js"
+  }
+]);
